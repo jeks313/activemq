75dbf535d795ef5a5a42a2f8547f98a52526f915
Christopher Hyde <chris.hyde@fusemail.com>
2020-02-22 05:32:39 -0800

    gitignore and package updates

--- producer/go.mod
+++ producer/go.mod
@@ -3,7 +3,9 @@ module github.com/jeks313/activemq/producer
 go 1.13
 
 require (
+	github.com/bxcodec/faker v2.0.1+incompatible
 	github.com/go-stomp/stomp v2.0.4+incompatible
+	github.com/google/uuid v1.1.1
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/tidwall/gjson v1.5.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
--- producer/main.go
+++ producer/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
+	"time"
 
+	"github.com/bxcodec/faker"
 	stomp "github.com/go-stomp/stomp"
+	"github.com/google/uuid"
 )
 
 //Connect to ActiveMQ and produce messages
@@ -21,13 +27,42 @@ func main() {
 	}
 }
 
+func randomUUIDs(num int) []string {
+	var uuids []string
+	for i := 0; i < num; i++ {
+		id := uuid.New()
+		log.Printf("created uuid: %s", id.String())
+		uuids = append(uuids, id.String())
+	}
+	return uuids
+}
+
+type fakey struct {
+	UUID        string
+	Name        string
+	Time        time.Time
+	SoftwareIDs []int
+	Model       string
+}
+
+const numUUIDs = 10
+
 func Producer(conn *stomp.Conn, count int) {
 	var n int
+	uuids := randomUUIDs(numUUIDs)
+	var d fakey
+	var err error
 	for {
-		err := conn.Send(
-			"/topic/VirtualTopic.Test",
-			"text/plain",
-			[]byte(fmt.Sprintf("Test message #%d", n)))
+		err = faker.FakeData(&d)
+		if err != nil {
+			log.Fatal("failed to fake my data")
+		}
+		d.UUID = uuids[rand.Intn(numUUIDs)]
+		b, err := json.Marshal(&d)
+		if err != nil {
+			log.Fatal("failed to convert fake doc to JSON")
+		}
+		err = conn.Send("/topic/VirtualTopic.Test", "text/plain", b)
 		n++
 		if err != nil {
 			fmt.Println(err)
